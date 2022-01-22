924744aedda72e4545d7c06378e1d8eda11a9e40
Lormenyo <lormenyoh@gmail.com>
2022-01-22 22:09:46 +0000

    FEAT: added retry feature for low bandwidth cases

--- workspaces_app/lib/services/workspace_service.dart
+++ workspaces_app/lib/services/workspace_service.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:retry/retry.dart';
 
 class WorkspaceService {
   static getWorkspaces() async {
-    print("Getting Workspaces");
     var url = Uri.parse('https://api.carbonvoice.app/challenge/workspaces');
-    final response = await http.get(url);
+    final retryOptions = RetryOptions(maxAttempts: 5);
+    final response = await retryOptions.retry(
+      // Make a GET request
+      () => http.get(url).timeout(Duration(seconds: 5)),
+      // Retry on SocketException or TimeoutException
+      retryIf: (e) => e is SocketException || e is TimeoutException,
+    );
 
     if (response.statusCode == 200) {
       var workspaceData = jsonDecode(response.body);
