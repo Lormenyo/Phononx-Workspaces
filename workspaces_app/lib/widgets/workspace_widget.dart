c8273bf7f450850cee68bcbc2b92d30c70745e62
Lormenyo <lormenyoh@gmail.com>
2022-01-22 15:17:08 +0000

    FEAT: added error checker for images

--- workspaces_app/lib/widgets/workspace_widget.dart
+++ workspaces_app/lib/widgets/workspace_widget.dart
@@ -13,21 +13,39 @@ class WorkspaceWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
       padding: EdgeInsets.symmetric(horizontal: 2, vertical: 10),
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(10)),
       child: Row(
         children: [
-          Container(
-            height: 50,
-            width: 100,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                    image: CachedNetworkImageProvider(this.imageUrl),
-                    onError: (_, e) {},
-                    fit: BoxFit.cover)),
+          // Container(
+          //   height: 50,
+          //   width: 100,
+          //   decoration: BoxDecoration(
+          //       borderRadius: BorderRadius.circular(10),
+          //       image: DecorationImage(
+          //           image: CachedNetworkImageProvider(this.imageUrl, ),
+          //           onError: (_, e) {},
+          //           fit: BoxFit.cover)),
+          // ),
+          CachedNetworkImage(
+            imageUrl: this.imageUrl,
+            imageBuilder: (context, imageProvider) => Container(
+                height: 50.0,
+                width: 100,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  fit: BoxFit.fitWidth,
+                  alignment: FractionalOffset.center,
+                  image: imageProvider,
+                ))),
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Container(
+              height: 50.0,
+              width: 100,
+              child: Icon(Icons.error),
+            ),
           ),
           SizedBox(
             width: 60,
